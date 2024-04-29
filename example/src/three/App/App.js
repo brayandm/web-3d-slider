14d2c5a6c497a9519dc6914c7d58062792cc00ca
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-04-29 19:33:33 +0200

    Add cube object and animation to App.js

--- example/src/three/App/App.js
+++ example/src/three/App/App.js
@@ -1,4 +1,11 @@
-import { PerspectiveCamera, WebGLRenderer } from "three";
+import {
+    BoxGeometry,
+    Mesh,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    Scene,
+    WebGLRenderer,
+} from "three";
 
 export default class App {
     _gl;
@@ -18,5 +25,25 @@ export default class App {
         // CAMERA
         const aspect = window.innerWidth / window.innerHeight;
         this._camera = new PerspectiveCamera(60, aspect, 1, 100);
+        this._camera.position.y = 0.8;
+        this._camera.position.z = 5;
+
+        // SCENE
+        this._scene = new Scene();
+
+        // OBJECT
+        const cubeGeometry = new BoxGeometry(1, 1, 1);
+
+        const cubeMaterial = new MeshBasicMaterial();
+
+        const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
+
+        this._scene.add(cubeMesh);
+
+        this._animate();
+    }
+
+    _animate() {
+        this._gl.render(this._scene, this._camera);
     }
 }
