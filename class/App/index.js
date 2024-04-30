d7e4f55c8e1a915a7eb9fa5603839bb431802f1b
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-04-30 19:33:24 +0200

    Update camera position and add moon and universe objects

--- class/App/index.js
+++ class/App/index.js
@@ -36,7 +36,8 @@ export default class App {
         const aspect = window.innerWidth / window.innerHeight;
         this._renderer.setSize(window.innerWidth, window.innerHeight);
         this._camera = new PerspectiveCamera(75, aspect, 1, 100);
-        this._camera.position.z = 5;
+        this._camera.position.z = 8;
+        this._camera.position.y = 3;
 
         this._scene = new Scene();
 
@@ -53,7 +54,7 @@ export default class App {
         const mesh = new Mesh(geometry, material);
         this._mesh = mesh;
 
-        const texture = new TextureLoader().load("App/assets/world.jpg");
+        const texture = new TextureLoader().load("App/assets/worldhd.jpg");
         texture.wrapS = texture.wrapT = RepeatWrapping;
         texture.repeat.set(1, 1);
 
@@ -64,18 +65,73 @@ export default class App {
             })
         );
 
+        const texture2 = new TextureLoader().load("App/assets/stars3.jpg");
+        texture2.wrapS = texture2.wrapT = RepeatWrapping;
+        texture2.repeat.set(10, 10);
+
+        this._universe = new Mesh(
+            new SphereGeometry(90, 40, 40),
+            new MeshBasicMaterial({
+                map: texture2,
+                side: BackSide,
+            })
+        );
+
+        this._moon = new Mesh(
+            new SphereGeometry(0.5, 40, 40),
+            new MeshBasicMaterial({
+                map: new TextureLoader().load("App/assets/moon.jpg"),
+            })
+        );
+
+        this._moon.position.x = 4;
+        this._moon.position.y = 1;
+
         // this._scene.add(this._mesh);
         this._scene.add(this._sphere);
+        this._scene.add(this._universe);
+        this._scene.add(this._moon);
 
         // START
         this._initEvents();
         this._start();
 
         this._animateSphere();
+        this._animateMoon();
+        this._animateUniverse();
     }
 
     _initEvents() {
         window.addEventListener("resize", this._onResize.bind(this));
+    }
+
+    _rotateMoon() {
+        this._moon.rotation.y += 0.01;
+    }
+
+    _rotateUniverse() {
+        this._universe.rotation.y += 0.0001;
+    }
+
+    _animateUniverse() {
+        window.requestAnimationFrame(() => {
+            this._rotateUniverse();
+            this._animateUniverse();
+        });
+    }
+
+    _rotateMoonAroundEarth() {
+        this._moon.position.x = Math.sin(Date.now() * 0.0005) * 3;
+        this._moon.position.z = Math.cos(Date.now() * 0.0005) * 3;
+        this._moon.position.y = Math.cos(Date.now() * 0.0005) * 3;
+    }
+
+    _animateMoon() {
+        window.requestAnimationFrame(() => {
+            this._rotateMoon();
+            this._rotateMoonAroundEarth();
+            this._animateMoon();
+        });
     }
 
     _moveSphere() {
