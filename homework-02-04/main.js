dd918bae5f58bfe01f5c84d5053eee76bce79244
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-03 15:26:51 +0200

    Add loading screen and show canvas after App initialization

--- homework-02-04/main.js
+++ homework-02-04/main.js
@@ -2,4 +2,10 @@ import "./style.css";
 
 import App from "./App/index.js";
 
-new App();
+const loading = document.querySelector(".loading-container");
+
+new App(() => {
+    loading.style.display = "none";
+    const canvas = document.querySelector("#canvas");
+    canvas.style.display = "block";
+});
