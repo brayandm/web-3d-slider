2afc7426b384947893cfeadffb0a649cf8fb0711
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-06 17:30:54 +0200

    Add GUIController and lil-gui library

--- weekly-01-02/main.js
+++ weekly-01-02/main.js
@@ -2,6 +2,8 @@ import "./style.css";
 
 import App from "./App/index.js";
 
+import GUIController from "./App/GUIController/index.js";
+
 const loading = document.querySelector(".loading-container");
 
 new App(() => {
@@ -9,3 +11,5 @@ new App(() => {
     const canvas = document.querySelector("#canvas");
     canvas.style.display = "block";
 });
+
+new GUIController();
