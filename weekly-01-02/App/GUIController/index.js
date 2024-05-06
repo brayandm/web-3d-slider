0ab2d2312e0cba07f0f2091bf91b081b2b8a6dc4
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-06 20:13:33 +0200

    Add postprocessing options to GUIController

--- weekly-01-02/App/GUIController/index.js
+++ weekly-01-02/App/GUIController/index.js
@@ -1,9 +1,16 @@
 import GUI from "lil-gui";
 
 export default class GUIController {
-    constructor(onHelpersChange = () => {}, onOrbitControllsChange = () => {}) {
+    constructor(
+        onHelpersChange = () => {},
+        onOrbitControllsChange = () => {},
+        onPostprocessingChange = () => {},
+        onPostprocessingCrazyChange = () => {}
+    ) {
         this._onHelpersChange = onHelpersChange;
         this._onOrbitControllsChange = onOrbitControllsChange;
+        this._onPostprocessingChange = onPostprocessingChange;
+        this._onPostprocessingCrazyChange = onPostprocessingCrazyChange;
         this.gui = new GUI();
         this.init();
     }
@@ -18,5 +25,15 @@ export default class GUIController {
             .add({ orbitControls: true }, "orbitControls")
             .name("Orbit Controls")
             .onChange(this._onOrbitControllsChange);
+
+        this.gui
+            .add({ postprocessing: true }, "postprocessing")
+            .name("Postprocessing")
+            .onChange(this._onPostprocessingChange);
+
+        this.gui
+            .add({ postprocessingCrazy: false }, "postprocessingCrazy")
+            .name("Postprocessing Crazy")
+            .onChange(this._onPostprocessingCrazyChange);
     }
 }
