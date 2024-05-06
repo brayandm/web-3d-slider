53372fcd5fb2ee675b513dbc61a7b0b3c2581588
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-06 20:13:38 +0200

    Add togglePostprocessingCrazy method to enable/disable crazy aberration effect

--- weekly-01-02/App/Postprocessing/index.js
+++ weekly-01-02/App/Postprocessing/index.js
@@ -13,6 +13,8 @@ export default class Postprocessing {
         this._camera = camera;
         this._clock = new Clock(true);
         this._nextUpdateTime = 0;
+        this._enableAberration = true;
+        this._enableAberrationCrazy = false;
 
         this._init();
     }
@@ -28,17 +30,17 @@ export default class Postprocessing {
 
         // EFFECTPASS
         this._chromaticAberrationEffect = new ChromaticAberrationEffect({
-            offset: new Vector2(0.005, 0.005),
+            offset: new Vector2(0.01, 0.01),
         });
 
-        const effectPass = new EffectPass(
+        this._effectPass = new EffectPass(
             this._camera,
             this._chromaticAberrationEffect
         );
 
         // ADD PASSES
         composer.addPass(renderPass);
-        composer.addPass(effectPass);
+        composer.addPass(this._effectPass);
 
         this._composer = composer;
     }
@@ -52,8 +54,26 @@ export default class Postprocessing {
         this._nextUpdateTime = this._clock.elapsedTime + timeInterval / 1000;
     }
 
+    togglePostprocessing(v) {
+        this._enableAberration = v;
+
+        if (this._enableAberration) {
+            this._chromaticAberrationEffect.offset.set(0.01, 0.01);
+        } else {
+            this._chromaticAberrationEffect.offset.set(0, 0);
+        }
+    }
+
+    togglePostprocessingCrazy(v) {
+        this._enableAberrationCrazy = v;
+    }
+
     render() {
-        if (this._clock.getElapsedTime() >= this._nextUpdateTime) {
+        if (
+            this._clock.getElapsedTime() >= this._nextUpdateTime &&
+            this._enableAberration &&
+            this._enableAberrationCrazy
+        ) {
             this._updateChromaticAberration();
         }
         this._composer.render();
