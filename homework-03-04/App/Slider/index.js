17969d6104b76d84bda624c4fcebcc04360db5e2
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-09 15:01:04 +0200

    Add dragging functionality to Slider component

--- homework-03-04/App/Slider/index.js
+++ homework-03-04/App/Slider/index.js
@@ -7,12 +7,17 @@ import {
     TextureLoader,
 } from "three";
 
+import { damp } from "maath/easing";
+
 export default class Slider extends Group {
     constructor() {
         super();
 
         this._height = 1920 * 0.3;
         this._width = 1080 * 0.3;
+        this._objects = [];
+
+        this._isDragging = false;
 
         this._init();
     }
@@ -33,7 +38,31 @@ export default class Slider extends Group {
             mesh.scale.set(this._width, this._height, 1);
             mesh.position.x = this._width * i * 1.5;
 
+            mesh.userData.destinationPosition = mesh.position.clone();
+            mesh.userData.initialPosition = mesh.position.clone();
+            mesh.userData.dragPosition = mesh.position.clone();
+
             this.add(mesh);
+            this._objects.push(mesh);
         }
+    }
+
+    onDrag(e, delta) {
+        this._isDragging = e.dragging;
+        this._objects.forEach((el) => {
+            el.userData.destinationPosition.x += delta;
+        });
+    }
+
+    update(delta) {
+        this._objects.forEach((el) => {
+            damp(
+                el.position,
+                "x",
+                el.userData.destinationPosition.x,
+                0.1,
+                delta
+            );
+        });
     }
 }
