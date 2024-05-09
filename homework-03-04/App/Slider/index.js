c112bfc5ae0fbd4a43fe2225c53026aae1d11f78
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-09 13:27:23 +0200

    Add slider functionality to create a grid of meshes

--- homework-03-04/App/Slider/index.js
+++ homework-03-04/App/Slider/index.js
@@ -1,11 +1,23 @@
-import { Group } from "three";
+import { Group, Mesh, MeshBasicMaterial, PlaneGeometry } from "three";
 
 export default class Slider extends Group {
     constructor() {
         super();
 
+        this._width = 50;
+
         this._init();
     }
 
-    _init() {}
+    _init() {
+        for (let i = 0; i < 12; i++) {
+            const geometry = new PlaneGeometry(1, 1);
+            const material = new MeshBasicMaterial();
+            const mesh = new Mesh(geometry, material);
+
+            mesh.scale.set(this._width, this._width, 1);
+
+            this.add(mesh);
+        }
+    }
 }
