998296f576b961fea10e89447947aaed09ef04e8
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-09 13:47:40 +0200

    Update Slider component with image loading functionality

--- homework-03-04/App/Slider/index.js
+++ homework-03-04/App/Slider/index.js
@@ -4,24 +4,33 @@ import {
     Mesh,
     MeshBasicMaterial,
     PlaneGeometry,
+    TextureLoader,
 } from "three";
 
 export default class Slider extends Group {
     constructor() {
         super();
 
-        this._width = 50;
+        this._height = 1920 * 0.3;
+        this._width = 1080 * 0.3;
 
         this._init();
     }
 
     _init() {
-        for (let i = 0; i < 12; i++) {
+        for (let i = 1; i <= 12; i++) {
             const geometry = new PlaneGeometry(1, 1);
-            const material = new MeshBasicMaterial();
+
+            const texture = new TextureLoader().load(
+                `./unsamples/image-` + i + `.jpg`
+            );
+
+            const material = new MeshBasicMaterial({
+                map: texture,
+            });
             const mesh = new Mesh(geometry, material);
 
-            mesh.scale.set(this._width, this._width, 1);
+            mesh.scale.set(this._width, this._height, 1);
             mesh.position.x = this._width * i * 1.5;
 
             this.add(mesh);
