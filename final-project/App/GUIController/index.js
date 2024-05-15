8846744ec737f8256633704a04cc5d4f1b1c36a0
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-15 20:00:32 +0200

    Remove unnecessary code in GUIController constructor

--- final-project/App/GUIController/index.js
+++ final-project/App/GUIController/index.js
@@ -5,7 +5,6 @@ export default class GUIController {
         this.config = config;
         this._changeConfiguration = changeConfiguration;
         this.gui = new GUI();
-        this.gui.close();
         this.init();
     }
 
